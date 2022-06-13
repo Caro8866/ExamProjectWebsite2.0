6e12b842b67ca4ab7d06d21dde565b1aedd93e91
Alessia Amore <a.amore9393@gmail.com>
2022-06-13 12:10:56 +0200

    related products working + remove carosel prod pg

--- scripts/productpage.js
+++ scripts/productpage.js
@@ -5,12 +5,20 @@ const id = urlParams.get("id");
 
 const url = "https://amorea.dk/WP-Exam/LeCaviste/wp-json/wp/v2/wine/" + id;
 
-//fetch the data
+//add second url for related products
+const url2 =
+  "https://amorea.dk/WP-Exam/LeCaviste/wp-json/wp/v2/wine?per_page=3&?_embed";
+
+//fetch the data for both
 fetch(url)
   .then((res) => res.json())
   .then((data) => showWine(data));
-//populate the page
 
+fetch(url2)
+  .then((res) => res.json())
+  .then((data) => handleProductList(data));
+
+//populate the page
 function showWine(wine) {
   console.log(wine);
   // name
@@ -53,4 +61,44 @@ function showWine(wine) {
 
   //     document.querySelector(".PPproductimg img").src =
   //       wine._links["wp:featuredmedia"][0].source_url;
+}
+
+//for each items in the related products list
+function handleProductList(data) {
+  //function needs different name from api.js to work
+  //console.log(data);
+  data.forEach(showProductList);
+}
+
+// function for related products
+
+function showProductList(wine) {
+  console.log(wine);
+  //grab template
+  const template = document.querySelector(".related_template").content;
+
+  //clone it
+  const myClone = template.cloneNode(true);
+  //change data
+
+  //name
+  myClone.querySelector(".relatedTitle").textContent = wine.title.rendered;
+
+  //link to specific product
+  myClone
+    .querySelector(".borderB")
+    .setAttribute("href", `productPage.html?id=${wine.id}`);
+  console.log(myClone.querySelector(".borderB"));
+
+  //price
+  myClone.querySelector(".relatedPrice").textContent = `DKK ${wine.price}`;
+
+  // //image
+  // myClone.querySelector(".relatedimg img").src =
+  //   wine["wp:featuredmedia"][0].source_url;
+
+  //select parent
+  const parent = document.querySelector(".related_parent_container");
+  //append child
+  parent.appendChild(myClone);
 }
